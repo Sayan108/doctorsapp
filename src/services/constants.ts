af92184a733418cecc191cc80aa9813b0d35d3d7
Sayan Dey <sayandeyriku@gmail.com>
2024-09-29 00:42:34 +0530

    updated api end points

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,24 +1,24 @@
 export const BaseURLs = {
-  // baseurl: "http://localhost:3000",
-  baseurl: "https://clientapp-backend.onrender.com/api/doctorapp",
+  baseURL: 'http://82.112.226.89:3001/api/doctorapp',
+  authBaseURL: 'http://82.112.226.89:3002/api/auth',
 };
 
 export const Endpoints = {
-  addAppointment: "/appointment/",
-  registerPatient: "/patient",
-  requestOTP: "/otp/",
-  login: "/user/login",
-  getAppointmentList: "/appointment/list",
-  clinicList: "/clinic/list",
-  clinicDetails: "/clinic/details",
-  availableSlots: "/clinic/getAvailableClinic",
-  doctorDetails: "/doctor/details",
-  addPatient: "/patient",
-  patientList: "/patient/patientlist",
-  updateUser: "/user",
-  appointmentDetails: "/appointment/details",
-  updateAppointment: "/appointment",
-  updatePatient: '/patient/update'
+  addAppointment: '/appointment/',
+  registerPatient: '/patient',
+  requestOTP: '/otp/',
+  login: '/user/login',
+  getAppointmentList: '/appointment/list',
+  clinicList: '/clinic/list',
+  clinicDetails: '/clinic/details',
+  availableSlots: '/clinic/getAvailableClinic',
+  doctorDetails: '/doctor/details',
+  addPatient: '/patient',
+  patientList: '/patient/patientlist',
+  updateUser: '/user',
+  appointmentDetails: '/appointment/details',
+  updateAppointment: '/appointment',
+  updatePatient: '/patient/update',
 };
 
-export const defaultDoctorId = "e0f64f05-3fa2-4356-b130-3aa30d3237b7";
+export const defaultDoctorId = 'e0f64f05-3fa2-4356-b130-3aa30d3237b7';
