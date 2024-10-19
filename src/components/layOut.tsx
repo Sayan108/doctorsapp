d8cd1f809f2bfb203176c7fda3b87bfef7ff7352
saikat <rick711c@gmail.com>
2024-10-19 12:57:33 +0530

    layout modified a little

--- src/components/layOut.tsx
+++ src/components/layOut.tsx
@@ -1,39 +1,59 @@
-import {View, StyleSheet} from 'react-native';
-import React from 'react';
-import {Appbar, Text} from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {theme} from '../theme/theme';
+import React from "react";
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { theme } from "../theme/theme";
+import { Text } from "react-native-paper";
 
 export interface Props {
   children: React.ReactNode;
-  navigation: any;
-  headerText: string;
+  navigation?: () => void;
+  headerText?: string;
+  hideBackButton?: boolean;
 }
 
 const Layout = (props: Props) => {
-  const {children, navigation, headerText} = props;
+  const { children, headerText } = props;
+  let hideBackButton = props.hideBackButton;
+  const navigation = useNavigation();
+
+  hideBackButton = hideBackButton ? hideBackButton : false;
+
   return (
-    <View style={[styles.container, {backgroundColor: theme.colors.surface}]}>
-      <View style={styles.headerContainer}>
-        <Appbar.Header
-          style={[styles.header, {backgroundColor: theme.colors.background}]}>
-          <Icon
-            name="arrow-left"
-            size={24}
-            color={theme.colors.onBackground}
-            onPress={navigation}
-            style={styles.icon}
-          />
-          <View style={styles.titleWrapper}>
-            <Text style={[styles.title, {color: theme.colors.onBackground}]}>
-              {headerText}
-            </Text>
+    <SafeAreaView style={[styles.container,{backgroundColor:theme.colors.surface}]}>
+      {/* Header */}
+      <View style={styles.header}>
+        {/* back button */}
+        {!hideBackButton && headerText ? (
+          <TouchableOpacity
+            onPress={() => {
+              navigation.goBack();
+            }}
+            style={styles.backButton}
+          >
+            <Icon name="arrow-back" size={24} color="black" />
+          </TouchableOpacity>
+        ) : null}
+
+        {/* header text */}
+        {headerText ? (
+          <View style={styles.headerTextContainer}>
+            <Text variant= 'titleMedium'>{headerText}</Text>
           </View>
-        </Appbar.Header>
+        ) : null}
       </View>
 
-      <View style={styles.contentContainer}>{children}</View>
-    </View>
+      {/* Content */}
+      <ScrollView contentContainerStyle={styles.content}>
+        {children}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -41,33 +61,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  headerContainer: {
-    zIndex: 1, // Ensure the header is on top
-  },
   header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    elevation: 2,
-    position: 'relative', // Make sure the header is positioned relative for absolute positioning inside it
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 15,
   },
-  icon: {
-    marginLeft: 8,
+  backButton: {
+    paddingRight: 20,
   },
-  titleWrapper: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  contentContainer: {
+  headerTextContainer: {
     flex: 1,
-    padding: 8, // Padding applied only to content
-    
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "black",
+  },
+  content: {
+    paddingTop: 10,
+    flexGrow: 1,
   },
 });
 
